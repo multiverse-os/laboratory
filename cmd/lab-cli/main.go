c5847a3ae31d158a160c3836b37ecbeb5b2382ab
Wade Welles <w.welles@multiverse-os.org>
2020-07-12 05:56:13 -0400

    just a little explanation until more README.md data can be written and the basic boilerplate scaffolding can be setup; this will also help speed up development in general for existing developers too; since we will be able to have code re find ourselves regularly writing generated automatically; and we can easily leverage existing code, see what needs to be done, and so we can get the alpha version online soon as possible

---
+++ cmd/lab-cli/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("[multiverse os] community development laboratory")
+	fmt.Println("================================================")
+	fmt.Println("Contained within this project is all the information")
+	fmt.Println("and all the tools you need to get started developing")
+	fmt.Println("on Multiverse OS; or developing tools or applications")
+	fmt.Println("for Multiverse OS. \n")
+	fmt.Println("A interrelated dependency graph, detailing which projects")
+	fmt.Println("rely on which projects; with details about what is required")
+	fmt.Println("estimated time to compleition, skill level, required skills")
+	fmt.Println("associated, and mentors/leads will all be found within this")
+	fmt.Println("project.\n\n Allowing anyone interested in volunteering or developing")
+	fmt.Println("to maximalize their invested time, making the most of their contributions")
+	fmt.Println("\n\nIt also sets up an easy to understand, and approachable framework for new")
+	fmt.Println("developers interested in learning more, and specificaly interested in")
+	fmt.Println("participating in open source software and hardware development")
+}
